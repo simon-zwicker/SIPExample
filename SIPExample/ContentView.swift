6367b2adc37cbd1d7f99c78ffb3a5bd74691f9f2
simon-zwicker <github@zwicker.io>
2023-11-20 10:45:58 +0100

    Initial Commit

---
+++ SIPExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SIPExample
+//
+//  Created by Simon Zwicker on 20.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SIPExample/SIPExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIPExampleApp.swift
+//  SIPExample
+//
+//  Created by Simon Zwicker on 20.11.23.
+//
+
+import SwiftUI
+
+@main
+struct SIPExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
